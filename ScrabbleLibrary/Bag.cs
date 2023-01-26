3698f19aef1999b9455d8b2a6814d1fa8ae0c9db
harryGIbong <tnstndico@gmail.com>
2023-01-26 17:50:11 -0500

    Harry_Process

--- ScrabbleLibrary/Bag.cs
+++ ScrabbleLibrary/Bag.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ScrabbleLibrary
 {
-    
+
     public class Bag : IBag
     {
 
@@ -15,29 +15,43 @@ namespace ScrabbleLibrary
         uint value;
         uint amount;
 
-        public enum letterEnum { a = 1, b = 3, c = 3, d = 2, e = 1, f = 4, g = 2, h = 4, i = 1, j = 8, k = 5, l = 1, m = 3, n = 1, o = 1, p = 3, q = 10, r = 1, s = 1, t = 1, u = 1, v = 4, w = 4, x = 8, y = 4, z = 10 }
-        Dictionary<letterEnum, int> letterMap = new Dictionary<letterEnum, int>() {
-        { letterEnum.a, 9},{ letterEnum.b, 2},{ letterEnum.c, 2},{ letterEnum.d, 4},
-        { letterEnum.e, 12},{ letterEnum.f, 2}, { letterEnum.g, 3}, { letterEnum.h, 2},
-        { letterEnum.i, 9}, {letterEnum.j, 1}, { letterEnum.k, 1}, { letterEnum.l, 4},
-        {letterEnum.m, 2}, { letterEnum.n, 6}, { letterEnum.o, 8}, { letterEnum.p, 2}, 
-        {letterEnum.q, 1}, { letterEnum.r, 6}, { letterEnum.s, 4}, { letterEnum.t, 6},
-        {letterEnum.u, 4}, { letterEnum.v, 2}, { letterEnum.w, 2}, { letterEnum.x, 1},
-        {letterEnum.y, 2}, { letterEnum.z, 1}};
+        //public enum letterEnum { a=0, b=1, c=2, d=3, e=4, f=5, g=6, h=7, i=8, j=9, k=10, l=11, m=12, n=13, o=14, p=15, q=16, r=17, s=18, t=19, u=20, v=21, w=22, x=23, y=24, z=25}
+        internal Dictionary<char, int> letterMap;
 
         string IBag.Author { get; init; }
-        uint IBag.TileCount { get; init; } = 98;
 
+        //get iterates through letterMap to calculate the #of tiles
+        uint IBag.TileCount
+        {
+            get
+            {
+                uint count = 0;
+                foreach (var tile in letterMap)
+                {
+                    count += (uint)tile.Value;
+                }
+                return count;
+            }
+            init { }
+        }
         public Bag()
         {
-            
+            letterMap = new Dictionary<char, int>() {
+        { 'a', 9},{ 'b', 2},{ 'c', 2},{ 'd', 4},
+        { 'e', 12},{ 'f', 2}, { 'g', 3}, { 'h', 2},
+        { 'i', 9}, {'j', 1}, { 'k', 1}, { 'l', 4},
+        {'m', 2}, { 'n', 6}, { 'o', 8}, { 'p', 2},
+        {'q', 1}, { 'r', 6}, { 's', 4}, { 't', 6},
+        {'u', 4}, { 'v', 2}, { 'w', 2}, { 'x', 1},
+        {'y', 2}, { 'z', 1}};
         }
+
 
 
 
         public IRack GenerateRack()
         {
-            throw new NotImplementedException();
+            return new Rack(this);
         }
     }
 }
--- ScrabbleLibrary/Rack.cs
+++ ScrabbleLibrary/Rack.cs
@@ -3,31 +3,121 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using static ScrabbleLibrary.Bag;
 
 namespace ScrabbleLibrary
 {
     internal class Rack : IRack
     {
-
+        private List<char> rackList = new();
+        Bag bag;
         public Rack(Bag bag)
         {
+            this.bag = bag;
+            Random r = new Random();
+            List<char> list = new List<char>();
+            for (int i = 0; i < 7; i++)
+            {
+                while (true)
+                {
+                    //0~25 represents each alphabet starting from 0 = A
+                    int rInt = r.Next(0, 25);
+                    KeyValuePair<char, int> pair = bag.letterMap.ElementAt(rInt);
+                    if (pair.Value > 0)
+                    {
+                        list.Add(pair.Key);
+                        //decrement  the tile count for randomly selected alphabet
+                        bag.letterMap[pair.Key]--;
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
 
+            }
         }
-        public uint TotalPoints { get; init; }
+
+
+
+        private uint _totalPoints;
+        public uint TotalPoints
+        {
+            get
+            {
+                return _totalPoints;
+            }
+            init
+            {
+                _totalPoints = value;
+            }
+        }
 
         public uint AddTiles()
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < 7 - rackList.Count; i++)
+            {
+
+            }
         }
 
+        public uint GetCharacterPoint(char character)
+        {
+            switch (character)
+            {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'l':
+                case 'n':
+                case 'o':
+                case 'r':
+                case 's':
+                case 't':
+                case 'u':
+                    return 1;
+                case 'd':
+                case 'g':
+                    return 2;
+                case 'b':
+                case 'c':
+                case 'm':
+                case 'p':
+                    return 3;
+                case 'f':
+                case 'h':
+                case 'v':
+                case 'w':
+                case 'y':
+                    return 4;
+                case 'k':
+                    return 5;
+                case 'j':
+                case 'x':
+                    return 8;
+                case 'q':
+                case 'z':
+                    return 10;
+                default:
+                    throw new NotImplementedException($"The alphabet {character} was not implemented.");
+            }
+        }
         public bool PlayWord(string candidate)
         {
-            throw new NotImplementedException();
+            Microsoft.Office.Interop.Word.Application wordObject = new();
+            return wordObject.CheckSpelling(candidate);
         }
 
         public uint TestWord(string candidate)
         {
-            throw new NotImplementedException();
+            uint score = 0;
+            foreach (char character in candidate)
+            {
+                score += GetCharacterPoint(character);
+            }
+            return score;
         }
     }
 }
